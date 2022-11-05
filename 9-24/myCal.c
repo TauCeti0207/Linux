c3879fc602fe74aab09e16b1d0a222423c0538eb
TauCeti0207 <2076188013@qq.com>
2022-11-05 22:24:50 +0800

    process_1105

---
+++ 9-24/myCal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>                                                                                                                         
+#include <string.h>
+#include <stdlib.h>
+// ./myCal a 10 20
+int main(int argc, char *argv[])
+{
+	if(argc == 4){
+        int x = atoi(argv[2]);
+        int y = atoi(argv[3]);
+		if(strcmp(argv[1], "-a") == 0){
+			 printf("%d+%d = %d\n", x,y,x+y);
+		}else if(strcmp(argv[1], "-s") == 0){
+			 printf("%d-%d = %d\n", x,y,x-y);
+		}else if(strcmp(argv[1], "-m") == 0){
+			 printf("%d*%d = %d\n", x,y,x*y);
+		}else if(strcmp(argv[1], "-d") == 0 && y != 0){
+            printf("%d/%d = %d\n", x,y,x/y);
+        }
+	}else{  
+    printf("please use ./myCal [-a|-s|-m|-d] one_data two_data\n");
+    }
+    return 0;
+}
