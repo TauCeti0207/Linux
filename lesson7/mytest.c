0b883803baed18ecf7ded320eca1e8239ff332ea
TauCeti0207 <2076188013@qq.com>
2023-04-28 00:51:41 +0800

    gcc_gdb_makefile_230428

---
+++ lesson7/mytest.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+        > File Name: mytest.c
+        > Author: tauceti0207
+        > Mail: 2076188013@qq.com
+        > Created Time: Fri Apr 28 00:36:08 2023
+ ************************************************************************/
+
+#include <stdio.h>
+int AddToTop(int top)
+{
+  int res = 0;
+  int i;
+  for(i = 1; i <= top; ++i)
+  {
+    res += i;
+  }
+  return res;
+}
+int main()
+{
+  int result = 0;
+  int top = 100;
+  result = AddToTop(top);
+  printf("result: %d\n", result);
+  return 0;
+}
---
+++ lesson7/test.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+        > File Name: test.cpp
+        > Author: tauceti0207
+        > Mail: 2076188013@qq.com
+        > Created Time: Thu Apr 27 00:04:52 2023
+ ************************************************************************/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using namespace std;
+int main()
+{
+    cout << "helloC++" << endl;
+    return 0;
+}
