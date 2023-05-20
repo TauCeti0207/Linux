e89800b72ce3ad1e933bb1e57382eb71439e46a9
TauCeti0207 <2076188013@qq.com>
2023-05-20 16:04:28 +0800

    230520_process_control

---
+++ lesson15_230520/ex6/mytest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+void fun()
+{
+
+}
+
+int main()
+{
+    printf("hello fun");
+    sleep(1);
+    exit(111);
+    return 10;
+}
