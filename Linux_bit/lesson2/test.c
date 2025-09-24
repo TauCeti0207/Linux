9df2762ca8b6b0126cbb8fd983a7cf68fb1dae7e
TauCeti0207 <2076188013@qq.com>
2025-09-25 00:03:43 +0800

    Auto commit for 2025-09-25

--- Linux_bit/lesson2/test.c
+++ Linux_bit/lesson2/test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
-{
-printf("hello world");
-return 0;
+int main() {
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+  printf("hello world\n");
+
+  printf("hello world\n");
+
+  return 0;
 }
